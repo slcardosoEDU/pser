57df91160373adf969d49fa5c4fe24b7617fc3aa
Samuel <slcardoso.docente@gmail.com>
2023-09-15 12:53:18 +0200

    main added.

--- src/ejemplos/crearhilos/Principal.java
+++ src/ejemplos/crearhilos/Principal.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejemplos.crearhilos;
 
 /**
  *
- * @author Samu
+ * @author Samuel
  */
 public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Soy el hilo principal.");
+    }
     
 }
