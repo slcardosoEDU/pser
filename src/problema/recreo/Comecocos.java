bb3611ceeb9e364e97b0477f67013f2d26528620
slcardosoEDU <101585061+slcardosoEDU@users.noreply.github.com>
2023-09-22 21:37:51 +0200

    Update Comecocos.java

--- src/problema/recreo/Comecocos.java
+++ src/problema/recreo/Comecocos.java
@@ -1,9 +1,57 @@
 package problema.recreo;
 
+
 /**
  * Reprenta el Comecocos que puede indicar mensajes.
  * @author Samuel Loureiro Cardoso
  */
-public class Comecocos {
+public class Comecocos extends Thread{
+    /**
+     * Posibles mensajes. Cada posición del array representa una cara del comecocos.
+     */
+    private final static String[] MENSAJES ={"Eres genial!",
+        "Lo vas a petar en PSER",
+        "Null pointer excecption :D",
+        "No es magia, es concurrencia",
+        "Soy el mensaje 5.",
+        "Te quedaste en la posición 5.",
+        "No es lo mismo 2 procesos paralelos que 2 procesos para lelos",
+        "Cuando llegas al final vuelves al principio"
+        };
+    /**
+     * El tiempo de espera para que el comecocos cambie de posición.
+     */
+    private final static int DELAY_MS = 200;
+    private int currentPos;
+    private boolean stop;
+    
+    public Comecocos(){
+        super();
+        currentPos = 0;
+        stop = false;
+    }
+    
+    @Override
+    public void run(){
+        while(!stop){
+            //Bonus track: un uso chulo del operador módulo para generar secuencias ciclicas de 
+            //números consecutivos. Se suele usar para recorrer arrays de forma
+            //circular.
+            currentPos = (currentPos+1)%MENSAJES.length;
+            try {
+                Thread.sleep(DELAY_MS);
+            } catch (InterruptedException ex) {
+                System.err.println(ex);
+            }
+            
+        }
+    }
+    
+    public void detener(){
+        stop = true;
+    }
+    public String getMensaje(){
+        return MENSAJES[currentPos];
+    }
     
 }
