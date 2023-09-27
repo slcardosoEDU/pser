94ad008c42e726caedad6e2e4a9b2aa3983104ba
slcardosoEDU <101585061+slcardosoEDU@users.noreply.github.com>
2023-09-27 17:14:38 +0200

    Create Contador.java

---
+++ src/ejercicios/ejercicio2/Contador.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+public class Contador {
+ private int contador;
+ private Random random;
+
+ public Contador() {
+   this.contador=0;
+   this.random=new Random();
+ }
+
+ public int getContador() {
+   return this.contador;
+ }
+
+ public void incrementa() {
+
+  contador++;
+  /*
+   int valor=this.contador;
+   try { Thread.sleep(random.nextInt(5)); }
+   catch (InterruptedException ex) {}
+   this.contador=valor+1;
+  */
+
+ }
+}
